a246b44c566de94f97925cca65ef74ff75f09f1a
Jermaine Covington <46759713+LJCovingtonJr@users.noreply.github.com>
2020-04-25 19:15:06 -0400

    Version 0.0.0.9004
    Including data for practice use.

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' BBC News Summaries
+#'
+#' A subset of a dataset containing 100 BBC News articles, alongside a human written summary and one generated using the Textrank algorithm
+#'
+#' @format A data frame. 100 observations of 3 variables:
+#' \describe{
+#'   \item{article}{the written article}
+#'   \item{summary}{the human written summary}
+#'   \item{textrank}{the textrank generated summary}
+#' }
+#' @source \url{https://www.kaggle.com/pariza/bbc-news-summary}
+"data"
